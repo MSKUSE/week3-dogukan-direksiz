cacc8ac4c9c436d0ec4e2cbff8282e46ac98436c
dogukan-direksiz <dogukandireksiz@posta.mu.edu.tr>
2025-03-06 13:07:55 +0300

    FinMinimum.java is created

--- week3/src/FindPrimes.java
+++ week3/src/FindPrimes.java
@@ -1,2 +1,24 @@
 public class FindPrimes {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Invalid number of arguments");
+        }else{
+            int number = Integer.parseInt(args[0]);
+
+            for (int i = 2; i <= number; i++) {
+                boolean isPrime = true;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    System.out.println(i);
+                }
+
+            }
+        }
+    }
+
 }
